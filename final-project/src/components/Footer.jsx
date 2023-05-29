4d1512ac40873800f8f64ff374a634811150dead
figofer <figoferdian18@gmail.com>
2023-05-29 20:01:34 +0700

    Menyelesaikan Komponen Footer

--- final-project/src/components/Footer.jsx
+++ final-project/src/components/Footer.jsx
@@ -1,21 +1,29 @@
 import React from "react";
-import { AiFillYoutube } from "react-icons/ai";
+import { AiFillYoutube,AiFillInstagram,AiFillTwitterCircle} from "react-icons/ai";
 import "./Style.css"
 import Logo from '../assets/logo.png';
 function Footer(){
     return(
         <>
         <footer>
-            <img src={Logo} alt="" />
-            <div className="nav-footer">
-                    <a href="">Home</a>
-                    <a href="">Artikel</a>
-                    <a href="">Donasi</a>
-                    <a href="">Kontak</a>
+            <div className="container-top">
+                <img src={Logo} alt="" />
+                <div className="nav-footer">
+                        <a href="">Home</a>
+                        <a href="">Artikel</a>
+                        <a href="">Donasi</a>
+                        <a href="">Kontak</a>
+                </div>
+                <div className="media">
+                    <AiFillYoutube className="icon"/>
+                    <AiFillInstagram className="icon"/>
+                    <AiFillTwitterCircle className="icon" />
+                </div>
             </div>
-            <div className="media">
-                <AiFillYoutube className="icon"/>
+            <div className="container-bottom">
+            <p>copyrights © 2023 FigoFerdyian. All rights reserved</p>
             </div>
+
         </footer>
         </>
     )
