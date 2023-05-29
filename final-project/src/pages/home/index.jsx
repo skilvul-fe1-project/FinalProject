009d363f8edd5ca6d67bf672d739cb9b74155cc4
figofer <figoferdian18@gmail.com>
2023-05-30 00:14:39 +0700

    Menyelesaikan Section Hero banner

--- final-project/src/pages/home/index.jsx
+++ final-project/src/pages/home/index.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 import { BsPlayFill} from "react-icons/bs";
 import { AiFillYoutube,AiFillInstagram,AiFillTwitterCircle,} from "react-icons/ai";
 import Hero from "../../assets/Hero.png"
 import "../home/style.css"
+
 function Home(){
     return(
         <>
-        <section className="hero">
-            <div className="hero-fr">
-                <div className="hero-media">
+        <section className="hero" >
+            <div className="hero-fr" data-aos="fade-right" data-aos-duration="2500">
+                <div className="hero-media" data-aos="fade-down" data-aos-duration="2500" data-aos-delay="50">
                     <AiFillYoutube className="hero-icon"/>
                     <AiFillInstagram className="hero-icon"/>
                     <AiFillTwitterCircle className="hero-icon" />
@@ -18,14 +21,14 @@ function Home(){
                 <div className="hero-text">
                     <h1>Save Environment  save our Planet Earth.</h1>
                     <p>The natural environment is important to human health, it was foremost. Nature is very importance</p>
-                    <div className="hero-link">
-                    <a href="">Take Action</a>
-                    <a href="" className="watch"><BsPlayFill className="hero-icon"/> watch</a>
+                    <div className="hero-link" >
+                    <a href=""className="action">Take Action</a>
+                    <a href="" className="watch"><BsPlayFill className="hero-icon"/> Watch</a>
                     </div>
                 </div>
             </div>
-            <div className="hero-img">
-                <img src={Hero} alt="" />
+            <div className="hero-img" >
+                <img src={Hero} alt="" data-aos="fade-left"  data-aos-duration="2500" data-aos-easing="ease-in-out" />
             </div>
 
         </section>
