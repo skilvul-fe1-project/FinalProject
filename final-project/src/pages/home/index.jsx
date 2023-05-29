ff704bebf2a6b2dd2a66958dcf2b373274928923
figofer <figoferdian18@gmail.com>
2023-05-30 02:30:57 +0700

    Membuat modal Vidio pada tombol Watch

--- final-project/src/pages/home/index.jsx
+++ final-project/src/pages/home/index.jsx
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { BsPlayFill } from "react-icons/bs";
+import { AiFillYoutube, AiFillInstagram, AiFillTwitterCircle } from "react-icons/ai";
+import Hero from "../../assets/Hero.png";
+import "../home/style.css";
+import ModalVideo from "react-modal-video";
+import "react-modal-video/scss/modal-video.scss";
+
 AOS.init();
-import { BsPlayFill} from "react-icons/bs";
-import { AiFillYoutube,AiFillInstagram,AiFillTwitterCircle,} from "react-icons/ai";
-import Hero from "../../assets/Hero.png"
-import "../home/style.css"
+
 
 function Home(){
+    const [isOpen, setIsOpen] = useState(false);
     return(
         <>
         <section className="hero" >
             <div className="hero-fr" data-aos="fade-right" data-aos-duration="2500">
                 <div className="hero-media" data-aos="fade-down" data-aos-duration="2500" data-aos-delay="50">
-                    <AiFillYoutube className="hero-icon"/>
-                    <AiFillInstagram className="hero-icon"/>
-                    <AiFillTwitterCircle className="hero-icon" />
+                <a href="https://www.instagram.com/ui.figo/" target="_blank"> <AiFillInstagram className="hero-icon"/></a>
+                    <a href="" target="_blank"><AiFillYoutube className="hero-icon"/></a>
+                    <a href="" target="_blank">  <AiFillTwitterCircle className="hero-icon" /></a>
                     
                 </div>
                 <div className="hero-text">
@@ -23,13 +28,15 @@ function Home(){
                     <p>The natural environment is important to human health, it was foremost. Nature is very importance</p>
                     <div className="hero-link" >
                     <a href=""className="action">Take Action</a>
-                    <a href="" className="watch"><BsPlayFill className="hero-icon"/> Watch</a>
+                    <a href="#" className="watch" onClick={() => setIsOpen(true)}>
+            <BsPlayFill className="hero-icon" /> Watch</a>
                     </div>
                 </div>
             </div>
-            <div className="hero-img" >
-                <img src={Hero} alt="" data-aos="fade-left"  data-aos-duration="2500" data-aos-easing="ease-in-out" />
-            </div>
+            <img src={Hero} alt="" data-aos="fade-left"  data-aos-duration="2500" data-aos-easing="ease-in-out" className="hero-img" />
+
+            <ModalVideo channel="youtube" isOpen={isOpen} videoId="zVxzft7T_tA" onClose={() => setIsOpen(false)}/>
+            
 
         </section>
         </>
