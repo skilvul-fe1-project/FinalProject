3e27d71d330751a21ba5d27de37b99678ca74a19
Brian Adriansyah <brianadri45@gmail.com>
2023-05-30 14:19:42 +0700

    test

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Artikel from "./pages/Artikel/ArtikelPage";
+import Footer from "./components/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header />
       <Artikel />
+      <Footer />
     </>
   );
 }
