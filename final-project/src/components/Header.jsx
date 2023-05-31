f6f586bbabcd066ef6f9f65b992afd2700c7ee46
figofer <figoferdian18@gmail.com>
2023-05-31 07:09:56 +0700

    Menambahkan sticky header

--- final-project/src/components/Header.jsx
+++ final-project/src/components/Header.jsx
@@ -1,35 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Logo from '../assets/logo.png';
 import './Style.css';
 
 function Header() {
-    const [isActive, setIsActive] = useState(false);
+    const [hamburger, openHamburger] = useState(false);
+    const [sticky, stickyActive] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                stickyActive(true);
+            } else {
+                stickyActive(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     const toggleMenu = () => {
-    setIsActive(!isActive);
+        openHamburger(!hamburger);
+    };
+
+    const closeMenu = () => {
+        openHamburger(false);
     };
 
     return (
-    <>
-    <header>
-        <div className="logo">
-            <img src={Logo} alt="Logo" />
-        </div>
-        <nav className={`navbar ${isActive ? 'active' : ''}`}>
-            <ul>
-                <li><a href="" className="pin">Home</a></li>
-                <li><a href="">Artikel</a></li>
-                <li><a href="">Donasi</a></li>
-                <li><a href="">Kontak</a></li>
-            </ul>
-        </nav>
-        <div className={`hamburger ${isActive ? 'active' : ''}`} onClick={toggleMenu}>
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
-        </div>
-    </header>
-    </>
+        <header className={sticky ? 'sticky' : ''}>
+            <div className="logo">
+                <img src={Logo} alt="Logo" />
+            </div>
+            <nav className={`navbar ${hamburger ? 'active' : ''}`}>
+                <ul>
+                    <li><a href="/" className="pin" onClick={closeMenu}>Home</a></li>
+                    <li><a href="/" className="pin" onClick={closeMenu}>Artikel</a></li>
+                    <li><a href="/" className="pin" onClick={closeMenu}>Donasi</a></li>
+                    <li><a href="/" className="pin" onClick={closeMenu}>Kontak</a></li>
+                </ul>
+            </nav>
+            <div className={`hamburger ${hamburger ? 'active' : ''}`} onClick={toggleMenu}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+        </header>
     );
 }
 
